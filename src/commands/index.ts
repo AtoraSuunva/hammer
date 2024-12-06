0431d98147a177755fa6873de259cde83bc927b3
AtoraSuunva <at@atora.dev>
2024-12-06 01:02:42 -0500

    fix: imported raw_message twice by accident

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,13 +1,14 @@
 import type { Toucan } from 'toucan-js'
 import type { Command, Component } from 'workers-discord'
 import type { CtxWithEnv } from '../env'
+import { raw_channel } from './raw_channel'
 import { raw_message } from './raw_message'
 import { raw_user } from './raw_user'
 
 export const commands: Command<CtxWithEnv, Request, Toucan>[] = [
+  raw_channel,
   raw_message,
   raw_user,
-  raw_message,
 ]
 
 export const components: Component<CtxWithEnv, Request, Toucan>[] = []
